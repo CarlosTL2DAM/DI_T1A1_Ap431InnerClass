8879c944314d670b8d92da80f821226bd2c0bdfe
Carlos <carlostorreslinan@gmail.es>
2021-10-04 09:25:43 +0200

    Ejercicio Ap431 Clases internas Implementamos las clases internas dentro de la clase Control Circle

--- src/DI_T1A1_431InnerClass/ControlCircle.java
+++ src/DI_T1A1_431InnerClass/ControlCircle.java
@@ -5,7 +5,6 @@ package DI_T1A1_431InnerClass;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -21,53 +20,77 @@ import javafx.stage.Stage;
  * @author usuario
  */
 public class ControlCircle extends Application {
-    
+
+    Circle circulo;
 
     @Override
-    public void start(Stage primaryStage){
+    public void start(Stage primaryStage) {
         //Creamos el nodo principal en este caso será un Pane
         Pane root = new Pane();
         //Creamos el circulo y lo ajustamos al centro, ponemos borde negro y color interno blanco
-        Circle circulo = new Circle();
+        circulo = new Circle();
         circulo.setRadius(25);
         circulo.centerXProperty().bind(root.widthProperty().divide(2));
         circulo.centerYProperty().bind(root.heightProperty().divide(2));
         circulo.setStroke(Color.BLACK);
         circulo.setFill(Color.WHITE);
-        
+
         //Hacemos que el circulo se añada en el root (Pane)
         root.getChildren().add(circulo);
-        
+
         //Creamos un boton que sirva para disminuir
         Button btnDisminuir = new Button("Shrink");
         btnDisminuir.setLayoutX(100);
         btnDisminuir.setLayoutY(250);
-        
+        btnDisminuir.setOnAction(new ButtonEventDisminuir());
+
         //Añadimos el botón
         root.getChildren().add(btnDisminuir);
-        
+
         //Creamos un botoón que sirva para agrandar
-        Button btnAgrandar = new Button ("Enlarge");
+        Button btnAgrandar = new Button("Enlarge");
         btnAgrandar.setLayoutX(250);
         btnAgrandar.setLayoutY(250);
+        btnAgrandar.setOnAction(new ButtonEventAgrandar());
+
         //Añadimos el botón
         root.getChildren().add(btnAgrandar);
 
-        //Instanciamos la clase de evento
-        ButtonEvent evento = new ButtonEvent(circulo, btnDisminuir, btnAgrandar);
-        btnAgrandar.setOnAction(evento);
-        btnDisminuir.setOnAction(evento);
         //Creamos la escena, la enlazamos con primaryStage y añadimos titulo
         Scene scene1 = new Scene(root, 400, 300);
         primaryStage.setScene(scene1);
         primaryStage.setTitle("Ejercicio 4.3.1");
-        
+
         //Mostramos la primaryStage
         primaryStage.show();
     }
-    
-            
 
+    class ButtonEventAgrandar implements EventHandler<ActionEvent> {
+
+        @Override
+        public void handle(ActionEvent evento) {
+            Agrandar(circulo);
+        }
+    }
+
+    class ButtonEventDisminuir implements EventHandler<ActionEvent> {
+
+        @Override
+        public void handle(ActionEvent evento) {
+            Disminuir(circulo);
+        }
+
+    }
+
+    //Metodo Agrandar
+    public void Agrandar(Circle circulo) {
+        circulo.setRadius(circulo.getRadius() * 2);
+    }
+
+    //Metodo Disminuir
+    public void Disminuir(Circle circulo) {
+        circulo.setRadius(circulo.getRadius() / 2);
+    }
 
     /**
      * @param args the command line arguments
@@ -75,5 +98,5 @@ public class ControlCircle extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
